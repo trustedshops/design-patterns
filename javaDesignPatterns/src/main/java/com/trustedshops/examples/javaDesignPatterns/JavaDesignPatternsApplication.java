7755cd353324065dd66f830a9a003f9ac1e52398
Thomas Volk <thomas.volk@trustedshops.de>
2017-08-01 16:45:35 +0200

    add java

---
+++ javaDesignPatterns/src/main/java/com/trustedshops/examples/javaDesignPatterns/JavaDesignPatternsApplication.java
@@ -0,0 +1,12 @@
+package com.trustedshops.examples.javaDesignPatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDesignPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDesignPatternsApplication.class, args);
+	}
+}
